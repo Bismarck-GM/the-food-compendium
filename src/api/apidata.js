80c62e171232f847f4c372cbe26e3744f4065c9d
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-16 22:21:22 -0300

    Edit name of constants. Add new function for new URL string.

--- src/api/apidata.js
+++ src/api/apidata.js
@@ -1,12 +1,14 @@
-const ALLMEALCATEGORIES = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+const ALLCATEGORIES = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 const SIMPLECATEGORIES = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 const RANDOMMEAL = 'www.themealdb.com/api/json/v1/1/random.php';
 
 const singleMeal = (id) => `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+const mealByCategoryURL = (category) => `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
 
 export {
-  ALLMEALCATEGORIES,
+  ALLCATEGORIES,
   SIMPLECATEGORIES,
   RANDOMMEAL,
   singleMeal,
+  mealByCategoryURL,
 };
