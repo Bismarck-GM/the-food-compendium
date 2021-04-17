419707f67635624b9b187678431bdb6a665f622f
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-16 22:22:14 -0300

    Edit. Add new logic for first API fetch. Change some styles.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,60 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import {
+  Box,
+  Skeleton,
+  Image,
+  Text,
+  Heading,
+} from '@chakra-ui/react';
 import { fetchCategories } from '../redux/actions';
 
 function Home({ fetchCategories, categories }) {
-  const { data, error, loading } = categories;
+  const { allCategories, error, loading } = categories;
 
-  console.log(categories);
-
-  useEffect(() => {
+  if (allCategories === null) {
     fetchCategories();
-  }, []);
+  }
 
   if (error) {
     return `Error: ${error} `;
   }
-
-  if (loading) {
-    return 'Loading...';
-  }
-
-  if (data) {
-    return (
-      <div className="App">
-        {`${data.categories}`}
-      </div>
-    );
-  }
-
   return (
-    <div className="App">
-      Tuvieja
-    </div>
+    <Box paddingTop="64px">
+      <Skeleton
+        isLoaded={!loading}
+        height="calc(100vh - 64px)"
+        margin={{ base: '0', lg: '0 auto' }}
+        display={{ lg: 'flex' }}
+        flexDirection={{ lg: 'column' }}
+        minWidth="100%"
+      >
+        { allCategories ? allCategories.map((cat) => (
+          <Box
+            key={cat.idCategory}
+            minWidth={['100%', '100%', '50%']}
+            backgroundColor="rgb(0,0,0,0.5)"
+            color="white"
+            position="relative"
+            alignSelf="center"
+          >
+            <Link
+              to={`categories/${cat.strCategory}`}
+            >
+              <Image src={cat.strCategoryThumb} alt={cat.strCategory} w="100%" />
+            </Link>
+            <Box backgroundColor="rgb(167,199,231,0.8)" position="absolute" bottom="0" w="100%" p={[2, 4, 6, 8]}>
+              <Heading size="lg" fontFamily="'Advent Pro', sans-serif;">
+                {`${cat.strCategory}`}
+              </Heading>
+              <Text fontFamily="'Advent Pro', sans-serif;" color="black">Theres X amount of recepies</Text>
+            </Box>
+          </Box>
+        )) : ''}
+      </Skeleton>
+    </Box>
   );
 }
 
@@ -44,7 +67,7 @@ const mapStateToProps = (state) => ({
 });
 
 Home.propTypes = {
-  categories: PropTypes.arrayOf(PropTypes.object).isRequired,
+  categories: PropTypes.objectOf(PropTypes.any).isRequired,
   fetchCategories: PropTypes.func.isRequired,
 };
 
