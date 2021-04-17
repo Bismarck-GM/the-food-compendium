ed7c8484ce7d47ee717bd5cae332eb4d18346d7e
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-17 08:20:45 -0300

    Add footer component.

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Flex, Image } from '@chakra-ui/react';
+
+const Footer = () => (
+  <Flex
+    justifyContent="center"
+    height={{ base: 8, lg: 12 }}
+    backgroundColor="black"
+    color="white"
+    position="fixed"
+    bottom="0"
+    w="100vw"
+    fontFamily="'Advent Pro', sans-serif;"
+  >
+    <Image src={`${process.env.PUBLIC_URL}/BismarckGMLogowhite-04.png`} />
+  </Flex>
+);
+
+export default Footer;
