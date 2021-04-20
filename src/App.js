397fffe1571039660c1f0aa7f11eb572494aec6b
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-20 14:46:36 -0300

    Add structural components for each Page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Box } from '@chakra-ui/react';
 import Home from './pages/Home';
 import Categories from './pages/Categories';
 import Navbar from './components/Navbar';
@@ -9,10 +10,20 @@ const App = () => (
   <>
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/categories/:category" exact component={Categories} />
-      </Switch>
+      <Box
+        d="flex"
+        flexDir="column"
+        alignItems="stretch"
+        justifyContent="center"
+        height="100vh"
+        paddingTop={{ base: 16, lg: 24 }}
+        paddingBottom={{ base: 8, lg: '48px' }}
+      >
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/categories/:category" exact component={Categories} />
+        </Switch>
+      </Box>
       <Footer />
     </Router>
   </>
