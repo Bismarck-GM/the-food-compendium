4912c7cf9c9d76330cdc743321ce40d69ce5a444
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-20 15:51:19 -0300

    Edit function name and export.

--- src/api/apidata.js
+++ src/api/apidata.js
@@ -2,13 +2,13 @@ const ALLCATEGORIES = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 const SIMPLECATEGORIES = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 const RANDOMMEAL = 'www.themealdb.com/api/json/v1/1/random.php';
 
-const singleMeal = (id) => `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+const singleRecipeURL = (id) => `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
 const mealByCategoryURL = (category) => `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
 
 export {
   ALLCATEGORIES,
   SIMPLECATEGORIES,
   RANDOMMEAL,
-  singleMeal,
+  singleRecipeURL,
   mealByCategoryURL,
 };
