8b1c88eaeaf7ed7ab9e9eabb3e5c6161c4ff8cc8
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-20 14:47:07 -0300

    Add drawer for menu. Edit some stylings to work properly.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import {
   Box,
   Flex,
   HStack,
   IconButton,
-  // Button,
-  // Menu,
-  // MenuList,
-  // MenuItem,
   useDisclosure,
-  useColorModeValue,
-  Stack,
-  // MenuButton,
+  // useColorModeValue,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  // Stack,
+  Text,
   Heading,
+  Link as Anchor,
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
-
-const Links = ['Home', 'Projects', 'Team'];
+import { FaGithub, FaHome } from 'react-icons/fa';
 
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  // const { allCategories, currentFilter } = categories;
   return (
     <>
-      <Box bg={useColorModeValue('rgb(167,199,231)', 'rgb(167,199,231)')} px={4} position="fixed" w="100%" zIndex="100">
-        <Flex h={16} alignItems="center" justifyContent="space-between">
+      <Box bgGradient="linear(to-r, red.500, yellow.500)" px={4} position="fixed" w="100%" zIndex="100" color="white" top="0">
+        <Flex h={{ base: 16, lg: 24 }} alignItems="center" justifyContent="space-between">
           <IconButton
             size="md"
-            variant="outline"
+            backgroundColor="inherit"
+            outline="none"
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
             aria-label="Open Menu"
             display={{ md: !isOpen ? 'none' : 'inherit' }}
@@ -38,7 +38,7 @@ const Navbar = () => {
           />
           <HStack spacing={8} alignItems="center">
             <Box>
-              <Heading fontFamily="'Butterfly Kids', cursive" size="lg">
+              <Heading fontFamily="'Butterfly Kids', cursive" size="2xl">
                 The
                 food
                 compendium
@@ -49,51 +49,47 @@ const Navbar = () => {
               spacing={4}
               display={{ base: 'none', md: 'flex' }}
             >
-              {Links.map((link) => (
-                <Link to="/" key={link}>{link}</Link>
-              ))}
+              <Link to="/" size="lg">
+                <FaHome style={{ display: 'inline', margin: '0 5px' }} />
+                Home
+              </Link>
+              <Anchor href="https://github.com/Bismarck-GM/" isExternal>
+                <FaGithub style={{ display: 'inline', margin: '0 5px' }} />
+                My Github
+              </Anchor>
             </HStack>
           </HStack>
-          {/* <Flex alignItems="center">
-            <Menu>
-              <MenuButton
-                as={Button}
-                variant="outline"
-                cursor="pointer"
-              >
-                {currentFilter}
-                <TriangleDownIcon
-                  size="sm"
-                />
-              </MenuButton>
-              <MenuList zIndex="101">
-                { allCategories ? allCategories.map((cat) => (
-                  <Link to={`categories/${cat.strCategory}`} key={cat.idCategory}>
-                    <MenuItem key={cat.idCategory}>{cat.strCategory}</MenuItem>
-                  </Link>
-                )) : ''}
-              </MenuList>
-            </Menu>
-          </Flex> */}
         </Flex>
-
         {isOpen ? (
-          <Box pb={4}>
-            <Stack as="nav" spacing={4}>
-              {Links.map((link) => (
-                <Link to="/" key={link}>{link}</Link>
-              ))}
-            </Stack>
-          </Box>
+          <Drawer onClose={onClose} isOpen={isOpen} size="full" placement="bottom">
+            <DrawerOverlay>
+              <DrawerContent bgGradient="linear(to-r, red.500, yellow.500)">
+                <DrawerHeader>
+                  <CloseIcon onClick={isOpen ? onClose : onOpen} />
+                </DrawerHeader>
+                <DrawerBody display="flex" flexDir="column" justifyContent="center" alignItems="center">
+                  <Link to="/" onClick={isOpen ? onClose : onOpen}>
+                    <Text fontSize="40px" textDecor="underline">
+                      <FaHome style={{ display: 'inline', margin: '0 5px' }} />
+                      Home
+                    </Text>
+                  </Link>
+                  <Anchor href="https://github.com/Bismarck-GM/" isExternal>
+                    <Text fontSize="40px" textDecor="underline">
+                      <FaGithub style={{ display: 'inline', margin: '0 5px' }} />
+                      My Github
+                    </Text>
+                  </Anchor>
+                </DrawerBody>
+              </DrawerContent>
+            </DrawerOverlay>
+          </Drawer>
+
         ) : null}
       </Box>
     </>
   );
 };
-
-// Navbar.propTypes = {
-//   categories: PropTypes.objectOf(PropTypes.any).isRequired,
-// };
 
 const mapStateToProps = (state) => ({
   categories: state.categories,
