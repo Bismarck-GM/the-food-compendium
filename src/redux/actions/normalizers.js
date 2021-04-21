220352eebf80fb8cd38cb10de032ac971d65d1ea
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-21 00:11:24 -0300

    Edit data normalizer for Recipe Steps. Strings are a mess in the API.

--- src/redux/actions/normalizers.js
+++ src/redux/actions/normalizers.js
@@ -5,7 +5,19 @@ export const normalizeDataByMeal = (apidata, category) => {
 };
 
 export const normalizeDataRecipe = (apidata, recipeId) => {
+  const newLocal = /[.]/g;
+  const regex = newLocal;
   const { meals } = apidata;
   const response = { [recipeId]: meals[0] };
+  response[recipeId].steppedInstructions = response[recipeId].strInstructions
+    .replace(/\n/gs, '')
+    .split(regex)
+    .map((string, i) => {
+      const nw = i + 1;
+      return {
+        instructionId: nw,
+        instruction: string.trim(),
+      };
+    });
   return response;
 };
