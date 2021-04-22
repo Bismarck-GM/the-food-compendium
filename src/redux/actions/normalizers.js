bb092b3b964f19ad21cd8307fadb036c3e44856d
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-22 02:08:23 -0300

    Edit. New assignment of api data. Discard not used objects and repeated keys.

--- src/redux/actions/normalizers.js
+++ src/redux/actions/normalizers.js
@@ -8,8 +8,8 @@ export const normalizeDataRecipe = (apidata, recipeId) => {
   const newLocal = /[.]/g;
   const regex = newLocal;
   const { meals } = apidata;
-  const response = { [recipeId]: meals[0] };
-  response[recipeId].steppedInstructions = response[recipeId].strInstructions
+  const response = { [recipeId]: {} };
+  response[recipeId].steppedInstructions = meals[0].strInstructions
     .replace(/\n/gs, '')
     .split(regex)
     .map((string, i) => {
@@ -17,23 +17,41 @@ export const normalizeDataRecipe = (apidata, recipeId) => {
       return {
         instructionId: nw,
         instruction: string.trim(),
+        isDone: false,
       };
     });
   const ingredientArray = [];
   for (let i = 1; i <= 20; i += 1) {
     const ingredientId = `strIngredient${i}`;
     const measureId = `strMeasure${i}`;
-    const ingredient = response[recipeId][ingredientId];
-    let measure = response[recipeId][measureId];
+    const ingredient = meals[0][ingredientId];
+    let measure = meals[0][measureId];
     if (measure.length === 1 || measure.match(/^\d+$/g)) {
       measure = `${measure} units`;
     }
     if (ingredient === '' || measure === '') {
       break;
     }
-    const ingredientItem = { id: i, ingredient, measure };
+    const ingredientItem = {
+      id: i,
+      ingredient,
+      measure,
+      isDone: false,
+    };
     ingredientArray.push(ingredientItem);
   }
   response[recipeId].ingredients = ingredientArray;
+  response[recipeId].dateModified = meals[0].dateModified;
+  response[recipeId].idMeal = meals[0].idMeal;
+  response[recipeId].strArea = meals[0].strArea;
+  response[recipeId].strCategory = meals[0].strCategory;
+  response[recipeId].strCreativeCommonsConfirmed = meals[0].strCreativeCommonsConfirmed;
+  response[recipeId].strDrinkAlternate = meals[0].strDrinkAlternate;
+  response[recipeId].strImageSource = meals[0].strImageSource;
+  response[recipeId].strMeal = meals[0].strMeal;
+  response[recipeId].strMealThumb = meals[0].strMealThumb;
+  response[recipeId].strSource = meals[0].strSource;
+  response[recipeId].strTags = meals[0].strTags;
+  response[recipeId].strYoutube = meals[0].strYoutube;
   return response;
 };
