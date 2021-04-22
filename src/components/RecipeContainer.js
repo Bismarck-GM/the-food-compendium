e1d9c42019153eae396a51545487a19e83445eb3
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-22 04:12:33 -0300

    Add to RecipeContainer. Masonry component. Step and ingredients togglers. More styles overall.

--- src/components/RecipeContainer.js
+++ src/components/RecipeContainer.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Masonry from 'react-masonry-css';
 import {
   Box,
   Text,
@@ -7,13 +9,18 @@ import {
   StackDivider,
   VStack,
   Fade,
+  // Grid,
+  GridItem,
 } from '@chakra-ui/react';
+import Separator from './Separator';
+import { toggleRecipeStep, toggleRecipeIngredient } from '../redux/actions';
 
-export default function RecipeContainer({ props }) {
+const RecipeContainer = (args) => {
+  const { toggleRecipeIngredient, toggleRecipeStep, props } = args;
   const { match: { params: { id: currentMealId } } } = props;
   const { recipes: { mobileCards } } = props;
   const currentRecipeId = currentMealId;
-  const { recipes: { allRecipes: { [currentRecipeId]: currentRecipe } } } = props;
+  const { recipes: { byId: { [currentRecipeId]: currentRecipe } } } = props;
 
   if (mobileCards.recipe) {
     return (
@@ -31,20 +38,32 @@ export default function RecipeContainer({ props }) {
           >
             {currentRecipe.strMeal}
           </Heading>
+          <Heading
+            size="md"
+            textAlign="center"
+            borderRadius="20px"
+            color="white"
+            fontFamily="'Cedarville Cursive', cursive"
+            mb={{ base: 6 }}
+            opacity="0.5"
+          >
+            {`${currentRecipe.strArea} Cuisine`}
+          </Heading>
           <VStack
             divider={<StackDivider borderColor="gray.200" />}
             spacing={6}
             align="stretch"
             justify="space-between"
           >
-            {currentRecipe.steppedInstructions.map((i) => (
+            {currentRecipe.steppedInstructions.map((step) => (
               <Text
-                key={i.instructionId}
+                key={step.instructionId}
                 color="white"
                 whiteSpace="pre-wrap"
                 fontFamily="'Open Sans', sans-serif;"
+                onClick={() => toggleRecipeStep(currentRecipe, step, step.isDone)}
               >
-                {i.instruction.length > 1 ? `${i.instructionId}. ${i.instruction}` : '' }
+                {step.instruction.length > 1 ? `${step.instructionId}. ${step.instruction}` : '' }
               </Text>
             ))}
           </VStack>
@@ -55,10 +74,8 @@ export default function RecipeContainer({ props }) {
 
   if (mobileCards.ingredients) {
     return (
-      <Fade in={!!mobileCards.ingredients} unmountOnExit>
-        <Box
-          h="100%"
-        >
+      <Fade in={!!mobileCards.ingredients}>
+        <Box>
           <Heading
             textAlign="center"
             borderRadius="20px"
@@ -69,6 +86,34 @@ export default function RecipeContainer({ props }) {
           >
             Ingredients
           </Heading>
+          <Masonry
+            breakpointCols={2}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+            {currentRecipe.ingredients.map((ing) => (
+              <GridItem
+                key={ing.id}
+                border={ing.isDone ? '1 px solid rgb(185,234,170)' : '1px solid white'}
+                color="white"
+                marginBottom="10px"
+                whiteSpace="pre-wrap"
+                fontFamily="'Open Sans', sans-serif;"
+                onClick={() => toggleRecipeIngredient(currentRecipe, ing, ing.isDone)}
+              >
+                <Box
+                  boxShadow="0 0 1rem 0 rgba(0, 0, 0, .5)"
+                  // backgroundColor="rgba(255, 255, 255, .1)"
+                  style={{ backdropFilter: 'blur(2px)' }}
+                >
+                  <Separator text={ing.measure} />
+                  <Text textAlign="center" fontSize="xl">
+                    {`${ing.ingredient}`}
+                  </Text>
+                </Box>
+              </GridItem>
+            ))}
+          </Masonry>
         </Box>
       </Fade>
     );
@@ -77,7 +122,9 @@ export default function RecipeContainer({ props }) {
   if (mobileCards.tools) {
     return 'Tools';
   }
-}
+
+  return 'Something went terribly wrong. :\'(';
+};
 
 RecipeContainer.defaultProps = {
   recipes: {},
@@ -89,3 +136,10 @@ RecipeContainer.propTypes = {
   recipes: PropTypes.objectOf(PropTypes.any),
   match: PropTypes.objectOf(PropTypes.any),
 };
+
+const mapDispatch = {
+  toggleRecipeStep,
+  toggleRecipeIngredient,
+};
+
+export default connect(null, mapDispatch)(RecipeContainer);
