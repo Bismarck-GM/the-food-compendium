453c2c57112452e14b474dcdf59d6fdf4e52350c
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-22 06:49:59 -0300

    Add gradient, transparency and padding to Home Page. Need to source better images.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,9 +31,24 @@ function Home({ fetchCategories, mealCategoryLoading, categories }) {
     >
       <Skeleton
         isLoaded={!loading}
+        position="relative"
         margin={{ base: '0', lg: '0 auto' }}
         minWidth="100%"
         minHeight={loading ? { base: 'calc(100vh - 88px)', lg: 'calc(100vh - 144px)' } : { lg: 'calc(100vh - 144px)' }}
+        backgroundColor="rgb(0,0,0,0.6)"
+        _after={{
+          content: '""',
+          bgGradient: 'linear(to-r, red.500, yellow.500)',
+          bgSize: 'cover',
+          bgRepeat: 'no-repeat',
+          opacity: '0.9',
+          top: '0',
+          left: '0',
+          bottom: '0',
+          right: '0',
+          position: 'absolute',
+          zIndex: '-1',
+        }}
       >
         <Heading backgroundColor="rgb(170, 151, 164, 0.8)" size="lg" fontFamily="'Advent Pro', sans-serif;" textAlign="center">What would you like to eat today?</Heading>
         <Box
@@ -42,31 +57,38 @@ function Home({ fetchCategories, mealCategoryLoading, categories }) {
           gridGap={{ md: '5px 5px', lg: '10px 10px' }}
           px={{ base: 0, md: 3, xl: 24 }}
           py={{ base: 0, lg: 6 }}
-          bgGradient="linear(0deg, rgba(213,213,213,1) 0%, rgba(246,246,246,1) 100%);"
+          // bgGradient="linear(0deg, rgba(213,213,213,1) 0%, rgba(246,246,246,1) 100%);"
           minHeight={{ lg: 'calc(100vh - 144px)' }}
         >
           { allCategories ? allCategories.map((cat) => (
             <Box
               key={cat.idCategory}
               minWidth={['100%', '100%', '50%']}
-              backgroundColor="rgb(127,127,127)"
+              backgroundColor="rgb(127,127,127,0.2)"
               color="white"
               position="relative"
               alignSelf="center"
               borderRadius={{ lg: '30px' }}
               boxShadow="dark-lg"
-              transition="all .2s ease-in-out"
+              transition="all .4s ease-in-out"
               _hover={{ transform: 'scale(1.05)', zIndex: '10' }}
+              padding={6}
             >
               <Link
                 to={`categories/${cat.strCategory}`}
               >
-                <Image src={cat.strCategoryThumb} alt={cat.strCategory} w="100%" borderRadius={{ lg: '30px' }} />
+                <Image
+                  src={cat.strCategoryThumb}
+                  alt={cat.strCategory}
+                  w="100%"
+                  borderRadius={{ lg: '30px' }}
+                />
               </Link>
               <Box
-                backgroundColor="rgb(255,117,85, 0.7)"
+                backgroundColor="rgb(215,150,47,0.7)"
                 position="absolute"
                 bottom="0"
+                left="0"
                 w="100%"
                 p={[2, 4]}
                 borderBottomRadius={{ lg: '30px' }}
@@ -74,7 +96,6 @@ function Home({ fetchCategories, mealCategoryLoading, categories }) {
                 <Heading size="md" fontFamily="'Advent Pro', sans-serif;">
                   {`${cat.strCategory}`}
                 </Heading>
-                {/* <Text fontFamily="'Advent Pro', sans-serif;" color="black"> of</Text> */}
               </Box>
             </Box>
           )) : ''}
