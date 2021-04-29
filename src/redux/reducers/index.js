f95ce346cc7f7bbd7cc723287ba3022c1a083b89
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-29 16:54:37 -0300

    Add Ingredients reducer. Edit reducers index, added Ingredients.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import mealCategoriesReducer from './Categories';
 import mealByCategoriesReducer from './mealsByCategory';
 import recipeReducer from './Recipe';
+import ingredientsReducer from './Ingredients';
 
 const rootStore = combineReducers({
   categories: mealCategoriesReducer,
   mealByCategory: mealByCategoriesReducer,
   recipes: recipeReducer,
+  ingredients: ingredientsReducer,
 });
 
 export default rootStore;
