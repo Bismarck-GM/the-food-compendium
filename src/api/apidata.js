8e2fbcadf894dcedbed3dbe3956db6df5a450a6c
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-29 16:55:19 -0300

    Edit Apidata. Add URL to fetch all ingredients.

--- src/api/apidata.js
+++ src/api/apidata.js
@@ -1,14 +1,16 @@
-const ALLCATEGORIES = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+const ALLCATEGORIESURL = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 const SIMPLECATEGORIES = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 const RANDOMMEAL = 'www.themealdb.com/api/json/v1/1/random.php';
+const ALLINGREDIENTSURL = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
 
 const singleRecipeURL = (id) => `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
 const mealByCategoryURL = (category) => `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
 
 export {
-  ALLCATEGORIES,
+  ALLCATEGORIESURL,
   SIMPLECATEGORIES,
   RANDOMMEAL,
   singleRecipeURL,
   mealByCategoryURL,
+  ALLINGREDIENTSURL,
 };
